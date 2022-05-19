25797666411251931e6deec00c586acfe8d4abe6
YazanAlhalabi <yazanalhalabi1@gmail.com>
2022-05-20 00:44:08 +0300

    init project

---
+++ api/src/app.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+// import lusca from 'lusca' will be used later
+import dotenv from 'dotenv'
+
+import movieRouter from './routers/movie'
+import apiErrorHandler from './middlewares/apiErrorHandler'
+import apiContentType from './middlewares/apiContentType'
+
+dotenv.config({ path: '.env' })
+const app = express()
+
+// Express configuration
+app.set('port', process.env.PORT || 3000)
+
+// Global middleware
+app.use(apiContentType)
+app.use(express.json())
+
+// Set up routers
+app.use('/api/v1/movies', movieRouter)
+
+// Custom API error handler
+app.use(apiErrorHandler)
+
+export default app
