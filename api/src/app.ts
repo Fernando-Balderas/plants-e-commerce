ffdf6e0601272fffb28ef0bdc45a6dada01a52a9
Fernando Balderas Guzmán <fernando.bguzman@gmail.com>
2022-05-23 14:55:29 +0300

    Move documentation to json based

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,14 +1,16 @@
+/* eslint @typescript-eslint/no-var-requires: "off" */
 import express from 'express'
 // import lusca from 'lusca' will be used later
 import dotenv from 'dotenv'
 import swaggerUi from 'swagger-ui-express'
 
-import swaggerSpec from './config/swagger'
 import userRouter from './routers/user'
 import productRouter from './routers/product'
 import orderRouter from './routers/order'
 import apiErrorHandler from './middlewares/apiErrorHandler'
 import apiContentType from './middlewares/apiContentType'
+const swaggerDocument = require('./docs/swagger-spec.json') // not using import due to an error
+// import swaggerSpec from './config/swagger'
 
 dotenv.config({ path: '.env' })
 const app = express()
@@ -21,7 +23,8 @@ app.use(apiContentType)
 app.use(express.json())
 
 // Set up routers
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/products', productRouter)
 app.use('/api/v1/orders', orderRouter)
