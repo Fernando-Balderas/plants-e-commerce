ff83d86366c5b3ae84ad2a6ef974e2ada110e1b9
Fernando Balderas Guzmán <fernando.bguzman@gmail.com>
2022-07-04 21:22:58 +0300

    Fix client test

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import GlobalFooter from './components/GlobalFooter'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders footer', () => {
+  render(<GlobalFooter />)
+  const element = screen.getByText(/Support local farmers./i)
+  expect(element).toBeInTheDocument()
+})
